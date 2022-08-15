975a47674c87e0e965bc473db718adf5f9c63287
Tal Weiss <major.tal@gmail.com>
2022-08-15 12:35:05 +0200

    Added a __main__

--- play.py
+++ play.py
@@ -4,11 +4,20 @@ import openai
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+assert openai.api_key
 
-res = openai.Completion.create(
-            model="text-davinci-002",
-            prompt="my name is:",
-            temperature=0.6,
-        )
 
-print(res)
+def demo():
+    res = openai.Completion.create(
+                model="text-davinci-002",
+                prompt="my name is:",
+                temperature=0.6,
+            )
+    print(res)
+
+
+
+
+print("here")
+if __name__ == "__main__":
+    demo()
