41276d439badb31f21a2128b76a74c23592fa990
Tal Weiss <major.tal@gmail.com>
2022-08-19 13:24:00 +0200

    Managed to evaluate the boolq noop predictions

--- solve_superglue.py
+++ solve_superglue.py
@@ -1,20 +1,36 @@
 from datasets import load_dataset_builder, load_dataset
+from evaluate import load as evaluate_load
 
 
 def process_datum(idx, question, passage, label):
         print(passage)
-        title, passage = passage.split(" -- ")
-        print(title)
-        print(passage)
-        exit()
+        passage_split = passage.split(" -- ")
+        title = passage_split[0]
+        passage = " -- ".join(passage_split[1:])
+        # print(title)
+        # print(passage)
+        prediction = 1
+        return prediction, label
 
 
 
 def main():
+    super_glue_metric_boolq = evaluate_load('super_glue', 'boolq') 
     train_dataset = load_dataset("super_glue", "boolq", split="train")
+    predictions = []
+    references = []
+
     for datum in train_dataset:
-        process_datum(**datum)
+        prediction, reference = process_datum(**datum)
+        predictions.append(prediction)
+        references.append(reference)
+
+    results = super_glue_metric_boolq.compute(predictions=predictions, references=references)
+
+    print(results)
 
 
 if __name__ == "__main__":
     main()
+
+
