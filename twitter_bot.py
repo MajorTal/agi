09997cfaeaa3724d20d5b8470ab5f3c3caf03556
Tal Weiss <major.tal@gmail.com>
2022-09-28 07:40:11 +0200

    bearer token in environment variable

--- twitter_bot.py
+++ twitter_bot.py
@@ -18,13 +18,14 @@ API_KEY = os.getenv("API_KEY")
 API_KEY_SECRET = os.getenv("API_KEY_SECRET")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
 
 # This works!!!
 auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
 api = tweepy.API(auth)
-TWITTER_CLIENT = tweepy.Client(bearer_token = "AAAAAAAAAAAAAAAAAAAAAPi2gwEAAAAACxUT%2B6fCyr3z8jAzDM0thADjfog%3D4M97crBo5Y5Yu9S3PHBiwL6VvYysJIdwaSueODAIlEL9WluY64"
-,access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET,consumer_key=API_KEY, consumer_secret=API_KEY_SECRET)
+TWITTER_CLIENT = tweepy.Client(bearer_token=BEARER_TOKEN, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET,consumer_key=API_KEY, consumer_secret=API_KEY_SECRET)
 data_dict = shelve.open("mentions_dict.pkl")
 
 # AWS:
